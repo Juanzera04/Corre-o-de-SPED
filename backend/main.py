8d1e28f865f8fce9c99e0279f261dd234eeb347e
Juanzera04 <juanrodriguessilv4@gmail.com>
2026-02-05 17:32:32 -0300

    Tentando ajustar o Deploy

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,18 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
-from fastapi.middleware.cors import CORSMiddleware
+# main.py
+
+from fastapi import FastAPI
 from sped_processor import processar_sped, consolidar_c175
-import uuid
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# ===============================
-# PIS / COFINS
-# ===============================
 
 @app.post("/corrigir")
-async def corrigir_sped(file: UploadFile = File(...)):
+def corrigir_sped(dados: list):
+    resultado = processar_sped(dados)
+    return {"resultado": resultado}
 
-    uid = str(uuid.uuid4())
-    entrada = f"/tmp/{uid}.txt"
-    saida = f"/tmp/{uid}_corrigido.txt"
-
-    with open(entrada, "wb") as f:
-        f.write(await file.read())
-
-    processar_sped(entrada, saida)
-
-    return FileResponse(
-        saida,
-        filename="SPED_PISCOFINS.txt",
-        media_type="text/plain"
-    )
-
-# ===============================
-# CONSOLIDAR C175
-# ===============================
 
 @app.post("/consolidar-c175")
-async def consolidar(file: UploadFile = File(...)):
-
-    uid = str(uuid.uuid4())
-    entrada = f"/tmp/{uid}.txt"
-    saida = f"/tmp/{uid}_c175.txt"
-
-    with open(entrada, "wb") as f:
-        f.write(await file.read())
-
-    consolidar_c175(entrada, saida)
-
-    return FileResponse(
-        saida,
-        filename="SPED_C175.txt",
-        media_type="text/plain"
-    )
+def consolidar(dados: list):
+    resultado = consolidar_c175(dados)
+    return {"resultado": resultado}
